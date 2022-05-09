6dc80663c4db18faf8c9bf130cc3e17bbac44d68
xmati <93664892+yt-xmati@users.noreply.github.com>
2022-05-09 19:08:05 +0200

    Update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -1,8 +1,5 @@
 package;
 
-import flixel.FlxG;
-import flixel.tweens.FlxEase;
-import flixel.tweens.FlxTween;
 import flixel.FlxSprite;
 import openfl.utils.Assets as OpenFlAssets;
 
@@ -10,38 +7,34 @@ using StringTools;
 
 class HealthIcon extends FlxSprite
 {
+	public var sprTracker:FlxSprite;
 	private var isOldIcon:Bool = false;
 	private var isPlayer:Bool = false;
 	private var char:String = '';
-	public var id:Int;
 
-	public var defualtIconScale:Float = 1;
-	public var iconScale:Float = 1;
-	public var iconSize:Float;
-
-	private var tween:FlxTween;
-
-	public function new(char:String = 'bf', isPlayer:Bool = false, ?_id:Int = -1)
+	public function new(char:String = 'bf', isPlayer:Bool = false)
 	{
 		super();
 		isOldIcon = (char == 'bf-old');
 		this.isPlayer = isPlayer;
 		changeIcon(char);
 		scrollFactor.set();
-
-		animation.play(char);
-		scrollFactor.set();
-
-		tween = FlxTween.tween(this, {}, 0);
 	}
 
+	override function update(elapsed:Float)
 	{
+		super.update(elapsed);
+
+		if (sprTracker != null)
+			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
+	}
 
 	public function swapOldIcon() {
 		if(isOldIcon = !isOldIcon) changeIcon('bf-old');
 		else changeIcon('bf');
 	}
 
+	private var iconOffsets:Array<Float> = [0, 0];
 	public function changeIcon(char:String) {
 		if(this.char != char) {
 			var name:String = 'icons/' + char;
@@ -49,26 +42,31 @@ class HealthIcon extends FlxSprite
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
 			var file:Dynamic = Paths.image(name);
 
-			loadGraphic(file, true, 150, 150);
-			animation.add(char, [0, 1, 2], 0, false, isPlayer);
+			loadGraphic(file); //Load stupidly first for getting the file size
+			loadGraphic(file, true, Math.floor(width / 2), Math.floor(height)); //Then load it fr
+			iconOffsets[0] = (width - 150) / 2;
+			iconOffsets[1] = (width - 150) / 2;
+			updateHitbox();
+
+			animation.add(char, [0, 1], 0, false, isPlayer);
 			animation.play(char);
 			this.char = char;
 
-			//antialiasing = ClientPrefs.globalAntialiasing;
-			//if(char.endsWith('-pixel') || char == 'bambi-3d' || char == 'dave-3d' || char == 'bambi-unfair' || char == 'bambi-god' || char == 'gary' || char == 'expunged' || char == 'bombu' || char == 'bamburg' || char == 'bamburg-player' || char == 'bambi-piss-3d') {
-				//antialiasing = false;
+			antialiasing = ClientPrefs.globalAntialiasing;
+			if(char.endsWith('-pixel')) {
+				antialiasing = false;
 			}
 		}
 	}
 
-	public function getCharacter():String {
-		return char;
+	override function updateHitbox()
+	{
+		super.updateHitbox();
+		offset.x = iconOffsets[0];
+		offset.y = iconOffsets[1];
 	}
 
-	public function tweenToDefaultScale(_time:Float, _ease:Null<flixel.tweens.EaseFunction>){
-
-		tween.cancel();
-		tween = FlxTween.tween(this, {iconScale: this.defualtIconScale}, _time, {ease: _ease});
-
+	public function getCharacter():String {
+		return char;
 	}
 }
