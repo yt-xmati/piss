baad857f10d7becceaecb473ae622d811f641008
xmati <93664892+yt-xmati@users.noreply.github.com>
2022-05-13 20:36:07 +0200

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -272,7 +272,7 @@ class MainMenuState extends MusicBeatState
 			else if (FlxG.keys.anyJustPressed(debugKeys))
 			{
 				selectedSomethin = true;
-				CoolUtil.browserLoad('https://cdn.discordapp.com/attachments/908164308452651039/972667757764153394/VID-20211107-WA0005.mp4'); //fuck you cheat!
+//				CoolUtil.browserLoad('https://cdn.discordapp.com/attachments/908164308452651039/972667757764153394/VID-20211107-WA0005.mp4'); //fuck you cheat!
                                 MusicBeatState.switchState(new SusState());
 			}
 			#end
