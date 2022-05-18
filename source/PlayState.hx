03347d7b8c9173fd55fdd7b1acf8c39cf14bfb70
xmati <93664892+yt-xmati@users.noreply.github.com>
2022-05-19 00:50:35 +0200

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1056,6 +1056,7 @@ class PlayState extends MusicBeatState
 		scoreTxt.cameras = [camHUD];
 		botplayTxt.cameras = [camHUD];
 		timeTxt.cameras = [camHUD];
+		timeBar.cameras = [camHUD];
 		swagWatermark.cameras = [camHUD];
 		doof.cameras = [camHUD];
 
